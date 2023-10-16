1e6d28a0b35e4624f53ea5f81cd7bf7dd5caae0c
zzrose <zz134124@outlook.com>
2023-10-16 17:37:35 -0400

    change icon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,6 +6,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Google Clone Next 13',
   description: 'Google Clone create by Next js 13',
+  icons: {
+    icon: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({ children }) {
