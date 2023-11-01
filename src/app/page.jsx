d580b066d2bb1e3e7224a2f68ea15bcf534df638
zzrose <zz134124@outlook.com>
2023-11-01 11:13:36 -0400

    add home header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HomeHeader from './components/HomeHeader'
 
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <HomeHeader />
+    </>
   )
 }
