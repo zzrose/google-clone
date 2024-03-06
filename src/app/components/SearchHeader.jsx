acc45df4a6cd5939a463fe29f2f1721ae300873e
zzrose <zz134124@outlook.com>
2024-03-06 12:53:42 -0500

    searchheader option complete

--- src/app/components/SearchHeader.jsx
+++ src/app/components/SearchHeader.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import SearchBox from './SearchBox'
 import { RiSettingsLine } from 'react-icons/ri'
 import { TbGridDots } from 'react-icons/tb'
+import SearchHeaderOptions from './SearchHeaderOptions'
 
 export default function SearchHeader() {
   return (
@@ -28,6 +29,7 @@ export default function SearchHeader() {
         </div>
         <button className='bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow ml-2'>Sign in</button>
       </div>
+      <SearchHeaderOptions />
     </header>
   )
 }
